4a5df07cf9f9814852d7aeb3a3c4a94786c2ccbc
kulakberkay15@gmail.com <kulakberkay15@gmail.com>
2022-01-07 07:19:39 -0700

    IUnitOfWork interface'sinin oluşturulması

--- AuthServer.Core/Dtos/CreateUserDto.cs
+++ AuthServer.Core/Dtos/CreateUserDto.cs
@@ -6,7 +6,10 @@ using System.Threading.Tasks;
 
 namespace AuthServer.Core.Dtos
 {
-    internal class CreateUserDto
+    public class CreateUserDto
     {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
     }
 }
