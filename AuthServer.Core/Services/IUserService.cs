002cf66c1da6651ff4deb4c9710668a091705958
kulakberkay15@gmail.com <kulakberkay15@gmail.com>
2022-01-07 07:13:02 -0700

    IUserService interface'sinin oluşturulması

---
+++ AuthServer.Core/Services/IUserService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AuthServer.Core.Dtos;
+using SharedLibrary.Dtos;
+
+namespace AuthServer.Core.Services
+{
+    // biz bunu AuthService.API projesinde controller içinde DI olarak geçeceğimiz için Responselar ile dönüyoruz.
+    public interface IUserService
+    {
+        // veritabanında oluşan kullanıcıyı dönücem. ID dahil olmak üzere
+        Task<Response<UserAppDto>> CreateUserAsync(CreateUserDto createUserDto);
+        Task<Response<UserAppDto>> GetUserByNameAsync(string userName);
+
+    }
+}
