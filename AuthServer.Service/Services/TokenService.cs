10e73cff78407063bb205eeeee9f834caa7cbb30
kulakberkay15@gmail.com <kulakberkay15@gmail.com>
2022-01-09 17:37:29 +0300

    TokenService -1

---
+++ AuthServer.Service/Services/TokenService.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using AuthServer.Core.Configuration;
+using AuthServer.Core.Dtos;
+using AuthServer.Core.Models;
+using AuthServer.Core.Services;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Options;
+using SharedLibrary.Configurations;
+
+namespace AuthServer.Service.Services
+{
+    public class TokenService:ITokenService
+    {
+        private readonly UserManager<UserApp> _userManager;
+        private readonly CustomTokenOptions _tokenOptions;
+
+        public TokenService(UserManager<UserApp> userManager, IOptions<CustomTokenOptions> tokenOptions)
+        {
+            _userManager = userManager;
+            _tokenOptions = tokenOptions.Value;
+        }
+
+        private string CreateRefreshToken()
+        {
+            var numberByte = new Byte[32];
+
+            using var rnd = RandomNumberGenerator.Create();
+
+            rnd.GetBytes(numberByte);
+
+            return Convert.ToBase64String(numberByte);
+        }
+
+        public TokenDto CreateToken(UserApp userApp)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ClientTokenDto CreateTokenByClient(Client client)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
